a6f646dc0d5a70a57710d1f9fca778098921904e
jerryzlau <jerryzlau@gmail.com>
2017-08-18 10:57:06 -0700

    Implemented my_attr_accessor

--- w3d5/active_record_lite/lib/00_attr_accessor_object.rb
+++ w3d5/active_record_lite/lib/00_attr_accessor_object.rb
@@ -1,5 +1,20 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
     # ...
+    names.each do |name|
+      instance_name = "@#{name.to_s}".to_sym
+      getter_name = "#{name.to_s}=".to_sym
+
+      #define get method
+      define_method(name) do
+        self.instance_variable_get(instance_name)
+      end 
+
+      #define setter method
+      define_method(getter_name) do |arg|
+        self.instance_variable_set(instance_name, arg)
+      end
+
+    end
   end
 end
