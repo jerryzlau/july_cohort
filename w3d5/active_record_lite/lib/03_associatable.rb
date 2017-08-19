6ced455feeefb64c1b27f434f90b5ff827185e4e
jerryzlau <jerryzlau@gmail.com>
2017-08-19 12:04:46 -0700

    Work on belongs_to in Assoiatable next

--- w3d5/active_record_lite/lib/03_associatable.rb
+++ w3d5/active_record_lite/lib/03_associatable.rb
@@ -11,22 +11,31 @@ class AssocOptions
 
   def model_class
     # ...
+    @class_name.constantize
   end
 
   def table_name
     # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
     # ...
+    @primary_key = (options[:primary_key] ||= :id)
+    @foreign_key = (options[:foreign_key] ||= "#{name}_id".to_sym)
+    @class_name = (options[:class_name] ||= name.capitalize)
+
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
     # ...
+    @primary_key = (options[:primary_key] ||= :id)
+    @foreign_key = (options[:foreign_key] ||= "#{self_class_name.downcase}_id".to_sym)
+    @class_name = (options[:class_name] ||= name[0..-2].capitalize)
   end
 end
 
@@ -34,6 +43,13 @@ module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
     # ...
+    options = BelongsToOptions.new(name, options)
+    define_method(name) do
+      foreign_k = self.send(options.foreign_key)
+      p self.class.superclass.where(primary_key: foreign_k)
+    end
+
+
   end
 
   def has_many(name, options = {})
@@ -47,4 +63,5 @@ end
 
 class SQLObject
   # Mixin Associatable here...
+  extend Associatable
 end
