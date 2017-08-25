2b8b23e78d9188df290b723ae0b9b674219b3f95
jerryzlau <jerryzlau@gmail.com>
2017-08-25 11:36:02 -0700

    Created session for sign in

--- w4d5/music_app/app/controllers/sessions_controller.rb
+++ w4d5/music_app/app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+
+  def new
+    render :new
+  end
+
+  def create
+    username = params[:user][:email]
+    password = params[:user][:password]
+    @user = User.find_by_credentials(username, password)
+    if @user
+      render json: "Welcome back #{@user.email}!"
+    else
+      flash.now[:errors] ||= []
+      flash[:errors] << "Invalid email or password"
+      render :new
+    end
+  end
+
+
 end
