d22ded18c5ded119bfcb85f367ad1daa4c26cf8a
jerryzlau <jerryzlau@gmail.com>
2017-08-25 14:35:39 -0700

    fix bugs in log out

--- w4d5/music_app/app/controllers/application_controller.rb
+++ w4d5/music_app/app/controllers/application_controller.rb
@@ -1,14 +1,12 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :log_out_user!
 
   def current_user
-    if session[:session_token]
-      @current_user ||= User.find_by(session_token: session[:session_token])
-    else
-      nil
-    end
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by(session_token: session[:session_token])
+
   end
 
   def logged_in?
