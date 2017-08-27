447749d85ae22eb10907a3696afb3cddbd2dd459
jerryzlau <jerryzlau@gmail.com>
2017-08-27 00:25:22 -0700

    Implemented integration test

--- w4d5/music_app/spec/features/auth_spec.rb
+++ w4d5/music_app/spec/features/auth_spec.rb
@@ -4,28 +4,32 @@ feature "the signup process" do
 
   scenario "has a new user page" do
     visit new_user_url
-    expect(page).to have_content "New user"
+    expect(page).to have_content "Sign up here!"
   end
 
   feature "signing up a user" do
     before(:each) do
       visit new_user_url
-      fill_in 'Email', :with => "testing@email.com"
-      fill_in 'password', :with => "biscuits"
-      click_on "Create User"
+      fill_in 'user[email]', :with => "testing@email.com"
+      fill_in 'user[password]', :with => "password"
+      click_on "create"
     end
 
-    scenario "redirects to bands index page after signup"
+    scenario "redirects to bands index page after signup" do
+      expect(page).to have_content "Sign in here!"
+    end
   end
 
   feature "with an invalid user" do
     before(:each) do
       visit new_user_url
-      fill_in 'Email', :with => "testing@email.com"
-      click_on "create user"
+      fill_in 'user[email]', :with => "testing@email.com"
+      click_on "create"
     end
 
-    scenario "re-renders the signup page after failed signup"
+    scenario "re-renders the signup page after failed signup" do
+      expect(page).to have_content "Sign up here!"
+    end
   end
 
 end
