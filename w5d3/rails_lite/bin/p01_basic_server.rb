68918692427ae0bac40216625b11b4685c7b6054
jerryzlau <jerryzlau@gmail.com>
2017-08-30 14:28:11 -0700

    Implemented Phase II

--- w5d3/rails_lite/bin/p01_basic_server.rb
+++ w5d3/rails_lite/bin/p01_basic_server.rb
@@ -1,7 +1,15 @@
 require 'rack'
 
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  res['Content-Type'] = 'text/html'
+  res.write(req.path_info)
+  res.write("she really sucks")
+  res.finish
+end
+
 Rack::Server.start(
-  app: Proc.new do |env|
-    ['200', {'Content-Type' => 'text/html'}, ['hello world']]
-  end
+  app: app,
+  Port: 3000
 )
--- w5d3/rails_lite/bin/p02_controller_server.rb
+++ w5d3/rails_lite/bin/p02_controller_server.rb
@@ -9,7 +9,10 @@ class MyController < ControllerBase
       redirect_to("/cats")
     end
   end
+
 end
+
+
 app = Proc.new do |env|
   req = Rack::Request.new(env)
   res = Rack::Response.new
@@ -21,4 +24,3 @@ Rack::Server.start(
   app: app,
   Port: 3000
 )
-
