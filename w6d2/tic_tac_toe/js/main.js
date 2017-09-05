a1ee466e510f1e795fe6bcde4a40bebf5dca24f0
jerryzlau <jerryzlau@gmail.com>
2017-09-05 15:34:41 -0700

    Done with tic tac toe

--- w6d2/tic_tac_toe/js/main.js
+++ w6d2/tic_tac_toe/js/main.js
@@ -1,6 +1,9 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require('./ttt-view');// require appropriate file
+const Game = require('../src/game');// require appropriate file
 
 $( () => {
   // Your code here
+  const game = new Game();
+  const $grid = $('figure.ttt');
+  const view = new View(game, $grid);
 });
