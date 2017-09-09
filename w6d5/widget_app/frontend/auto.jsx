40098073e83ed06d3914ae4fe49da2925b7eb2c3
Jerry Lau <jerryzlau@gmail.com>
2017-09-09 16:02:16 -0700

    Implemented search auto

---
+++ w6d5/widget_app/frontend/auto.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+class AutoComplete extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {inputVal: "", names: []};
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  cap(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  handleSearch(e){
+    let inputVal = e.target.value;
+    let names; 
+    
+    if (inputVal === "") {
+      names = [];
+    }else{
+      names = this.props.name.filter(function(n){
+        return n.substring(0,inputVal.length) === inputVal;
+      });
+    }
+
+    this.setState({inputVal, names});
+  }
+
+  render(){
+    let {inputVal, names} = this.state;
+    return (
+      <div>
+        <br/>
+        <label>
+          Search Names: <input type="text" onChange={this.handleSearch} value={inputVal}></input>
+        </label>
+
+        <ul>
+          {
+            names.map(el => (
+              <li key={el}>{this.cap(el)}</li>
+            ))
+          }
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default AutoComplete;
