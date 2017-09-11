17f6a8c9a225c5620ec4bbd521dd0233bd8c9282
App Academy Student <student@appacademy.io>
2017-09-11 11:27:05 -0700

    initialized todo project

---
+++ w7d1/redux_todos/frontend/todo_redux.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", function(){
+  ReactDOM.render(<h1>Todo Apps</h1>, document.getElementById('root'));
+});
