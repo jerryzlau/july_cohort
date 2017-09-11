bcdbbfabf7d04548cee23e33c016eec5bb1535e9
App Academy Student <student@appacademy.io>
2017-09-11 14:15:13 -0700

    Finished Phase 2

--- w7d1/redux_todos/frontend/todo_redux.jsx
+++ w7d1/redux_todos/frontend/todo_redux.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store.js';
+import {receiveTodos, receiveTodo } from './actions/todo_actions';
+
+window.receiveTodos = receiveTodos;
+window.receiveTodo = receiveTodo;
 
 document.addEventListener("DOMContentLoaded", function(){
+  const store = configureStore();
+  window.store = store;
   ReactDOM.render(<h1>Todo Apps</h1>, document.getElementById('root'));
 });
