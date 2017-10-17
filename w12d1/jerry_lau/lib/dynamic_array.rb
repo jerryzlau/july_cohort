dbe53e6dfe17d621afa29f6bcae7893cde903bd3
Jerry Lau <jerryzlau@gmail.com>
2017-10-17 13:18:25 -0700

    submitting homework

--- w12d1/jerry_lau/lib/dynamic_array.rb
+++ w12d1/jerry_lau/lib/dynamic_array.rb
@@ -4,33 +4,61 @@ class DynamicArray
   attr_reader :length
 
   def initialize
+    @capacity = 8 
+    @store = StaticArray.new(@capacity)
     @length = 0
   end
 
   # O(1)
   def [](index)
-    
+    raise "index out of bounds" if index >= @length
+    @store[index]
   end
 
   # O(1)
   def []=(index, value)
+    raise "index out of bounds" if index >= @length
+    @store[index] = value 
   end
 
   # O(1)
   def pop
+    raise "index out of bounds" if @length == 0 
+    @store[@length - 1] = nil
+    @length -= 1
   end
 
   # O(1) ammortized; O(n) worst case. Variable because of the possible
   # resize.
   def push(val)
+    resize! if @length >= @capacity 
+    @store[@length] = val
+    @length += 1
   end
 
   # O(n): has to shift over all the elements.
   def shift
+    raise "index out of bounds" if @length == 0 
+    result = @store[0]
+    counter = 0 
+    (1..@length-1).each do |idx|
+      @store[counter] = @store[idx]
+      counter += 1
+    end 
+    @length -= 1 
+    result 
   end
 
   # O(n): has to shift over all the elements.
   def unshift(val)
+    resize! if @length >= @capacity 
+    array = Array.new(@capacity)
+    array[0] = val
+    (1..@capacity-1).each do |idx|
+      array[idx] = @store[idx-1]
+    end 
+    @length += 1
+    @store = array[0..@length]
   end
 
   protected
@@ -42,5 +70,12 @@ class DynamicArray
 
   # O(n): has to copy over all the elements to the new store.
   def resize!
+    array = Array.new(@length*2)
+    (0..@length-1).each do |idx|
+      array[idx] = @store[idx]
+    end 
+    @capacity *= 2
+    @store = array 
   end
 end
+
--- w12d1/jerry_lau/lib/ring_buffer.rb
+++ w12d1/jerry_lau/lib/ring_buffer.rb
@@ -4,30 +4,53 @@ class RingBuffer
   attr_reader :length
 
   def initialize
+    @capacity = 8 
+    @store = StaticArray.new(@capacity)
+    @length = 0
+    @start_idx = 0 
   end
 
   # O(1)
   def [](index)
+    raise "index out of bounds" if index >= @length
+    @store[index]
   end
 
   # O(1)
   def []=(index, val)
+    raise "index out of bounds" if index >= @length
+    @store[index] = val
   end
 
   # O(1)
   def pop
+    raise "index out of bounds" if @length == 0 
+    @store[@length-1] = nil
+    @length -= 1
   end
 
   # O(1) ammortized
   def push(val)
+    resize! if @length >= @capacity 
+    @store[@length] = val
+    @length += 1
   end
 
   # O(1)
   def shift
+    raise "index out of bounds" if @length == 0 
+    result = @store[@start_idx]
+    @start_idx = (@start_idx + 1) % @capacity
+    @length -= 1 
+    result 
   end
 
   # O(1) ammortized
   def unshift(val)
+    resize! if @length >= @capacity 
+    @store[(@start_idx -1)% @capacity] = val
+    @start_idx = (@start_idx - 1) % @capacity 
+    @length += 1
   end
 
   protected
@@ -38,5 +61,11 @@ class RingBuffer
   end
 
   def resize!
+    array = Array.new(@length*2)
+    (0..@length-1).each do |idx|
+      array[idx] = @store[idx]
+    end 
+    @capacity *= 2
+    @store = array 
   end
 end
